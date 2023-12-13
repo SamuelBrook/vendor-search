c616568782ca092bd406be4f1cf0587b5edc69ac
Samuel SB. Brook <sbrook@nhbs.com>
2023-12-13 14:06:55 +0000

    remove csv dislay

--- script.js
+++ script.js
@@ -28,56 +28,7 @@ fetch(csvUrl)
 
         let oneValues = Object.keys(counter).filter(key => counter[key] === 1);
 
-        
-        let csvDataRead= fs.readFileSync(csvFilePath, 'utf8');
-        let parsedData = Papa.parse(csvDataRead, {header: true});
-
-        oneValues.forEach(oneValue => {
-            parsedData.data.push({[columnToUpdate]: oneValue.toString()});
-        });
-
-        let updatedCsv = Papa.unparse(parsedData);
-        fs.writeFileSync(csvFilePath, updatedCsv,'utf8');
-
-        console.log("splitelementarr", lastElementArr);
-        console.log("uniquevalues", uniqueValues);
-        console.log("countedonce", countedOnce);
-
-
-        const displayCsv = () => {
-            let csvFilePath = 'search-results.csv';
-
-            // Read the existing CSV file
-            Papa.parse(csvFilePath, {
-                header: true,
-                complete: function(results) {
-                    displayCSV(results.data);
-                }
-            });
-
-            // Function to display CSV data in a table
-            function displayCSV(data) {
-                let table = document.getElementById('csvTable');
-                
-                // Create header row
-                let headerRow = table.getElementsByTagName('thead')[0].insertRow(0);
-                for (let key in data[0]) {
-                    let th = document.createElement('th');
-                    th.innerHTML = key;
-                    headerRow.appendChild(th);
-                }
-
-                // Create data rows
-                for (let i = 0; i < data.length; i++) {
-                    let row = table.getElementsByTagName('tbody')[0].insertRow(i);
-                    for (let key in data[i]) {
-                        let cell = row.insertCell(-1);
-                        cell.innerHTML = data[i][key];
-                    }
-                }
-            }
-        }
-        displayCsv();
+        console.log("countedonce", oneValues);
     })
 
 
