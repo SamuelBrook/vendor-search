26521fb239dcb74ef94117d23022126503176b0f
Samuel SB. Brook <sbrook@nhbs.com>
2023-12-13 09:46:41 +0000

    split rows

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const csvUrl = './search-results.csv';
+
+fetch(csvUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response not ok');
+        }
+        return response.text();
+    })
+    .then(csvData => {
+        const rows = csvData.split('\n'); // split by newline to get rows
+        console.log(rows);
+    })
+
+
 const output = document.getElementById('output');
 output.innerHTML = "hello world";
 
