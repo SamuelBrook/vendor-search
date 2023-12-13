feb0bab1d3ef3854e71b36a496f3f0031e2f15a5
Samuel SB. Brook <sbrook@nhbs.com>
2023-12-13 13:58:16 +0000

    display CSV on webpage

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const csvUrl = './search-results.csv';
 const columnToUpdate = 'Values';
 
-
-
 fetch(csvUrl)
     .then(response => {
         if (!response.ok) {
@@ -28,14 +26,14 @@ fetch(csvUrl)
             return acc;
         }, {});
 
-        let countedOnce = Object.keys(counter).filter(key => counter[key] === 1);
+        let oneValues = Object.keys(counter).filter(key => counter[key] === 1);
 
         
         let csvDataRead= fs.readFileSync(csvFilePath, 'utf8');
         let parsedData = Papa.parse(csvDataRead, {header: true});
 
-        countedOnce.forEach(count => {
-            parsedData.data.push({[columnToUpdate]: uniqueValues.toString()});
+        oneValues.forEach(oneValue => {
+            parsedData.data.push({[columnToUpdate]: oneValue.toString()});
         });
 
         let updatedCsv = Papa.unparse(parsedData);
@@ -44,6 +42,42 @@ fetch(csvUrl)
         console.log("splitelementarr", lastElementArr);
         console.log("uniquevalues", uniqueValues);
         console.log("countedonce", countedOnce);
+
+
+        const displayCsv = () => {
+            let csvFilePath = 'search-results.csv';
+
+            // Read the existing CSV file
+            Papa.parse(csvFilePath, {
+                header: true,
+                complete: function(results) {
+                    displayCSV(results.data);
+                }
+            });
+
+            // Function to display CSV data in a table
+            function displayCSV(data) {
+                let table = document.getElementById('csvTable');
+                
+                // Create header row
+                let headerRow = table.getElementsByTagName('thead')[0].insertRow(0);
+                for (let key in data[0]) {
+                    let th = document.createElement('th');
+                    th.innerHTML = key;
+                    headerRow.appendChild(th);
+                }
+
+                // Create data rows
+                for (let i = 0; i < data.length; i++) {
+                    let row = table.getElementsByTagName('tbody')[0].insertRow(i);
+                    for (let key in data[i]) {
+                        let cell = row.insertCell(-1);
+                        cell.innerHTML = data[i][key];
+                    }
+                }
+            }
+        }
+        displayCsv();
     })
 
 
